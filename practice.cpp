41f55141839b74b4e613563107f5a58b395071f7
VikasVk03 <vishwakarmavikas0102@gmail.com>
2025-12-15 15:05:00 +0000

    Initial C++ commit

---
+++ practice.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+
+    int uncles = 5;
+    int aunts = uncles;
+    int chairs = aunts + uncles + 4;
+    unsigned quaterbreak;
+
+
+    cout << quaterbreak << endl;
+    
+    int hamburgers = {24};
+
+    int rocs (7);
+    int psychics {7}; 
+
+    int wrens (432); // alternative C++ syntax, set wrens to 432  
+
+    cout << uncles << endl << aunts << endl << chairs << endl;
+    cout << "hamburger " << hamburgers << endl;
+    cout << wrens;
+    return 0;
+}
