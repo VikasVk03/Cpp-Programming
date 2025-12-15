55743218fe8d2919adea48ece9ef2e8f79f1e1b6
VikasVk03 <vishwakarmavikas0102@gmail.com>
2025-12-15 15:12:05 +0000

    Initial C++ commit

---
+++ debug.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 10;
+    int b = 20;
+    int sum = a + b;
+
+    cout << "Sum = " << sum << endl;
+
+    sum += 5;
+    cout << "Updated Sum = " << sum << endl;
+
+    return 0;
+}
