87133ecdbaa95932215442ea60b50dd406df262f
VikasVk03 <vishwakarmavikas0102@gmail.com>
2025-12-16 12:03:12 +0000

    learn about limit of integer types and how they behave when limits are exceeded

---
+++ 02-DataTypes/01Limits.cpp
@@ -0,0 +1,32 @@
+// limits.cpp -- some limits of fundamental integer limits
+#include <iostream> 
+#include <climits>      // climits header file contains  information about integer type limits.
+
+int main ()
+{
+    using namespace std;
+    int n_int = INT_MAX;  // initialize n_init to max int value
+    short n_short = SHRT_MAX;
+    long n_long = LONG_MAX;
+    long long n_llong = LLONG_MAX;
+
+    // sizeof operator yields (or produce) size of type or of variable
+    cout << "int is " << sizeof (int) << " bytes." << endl;
+    cout << "short is " << sizeof n_short << " bytes." << endl;
+    cout << " long is " << sizeof n_long << " bytes." << endl;
+    cout << "long long is " << sizeof n_llong << " bytes." << endl;
+    cout << endl;
+
+
+    cout << "Maximum values: " << endl;
+    cout << "int: " << n_int << endl;
+    cout << "short: " << n_short << endl;
+    cout << "long: " << n_long << endl;
+    cout << "long long: " << n_llong << endl << endl;
+
+    cout << "Minimum int value = " << INT_MIN << endl;
+    cout << "Bits per byte = " << CHAR_BIT << endl;
+
+    return 0;
+
+}
