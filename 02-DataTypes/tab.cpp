912e7ad4ec0803460a460e501d3255dc6c599719
VikasVk03 <vishwakarmavikas0102@gmail.com>
2025-12-16 12:46:31 +0000

    horizontal and vertical tab example

---
+++ 02-DataTypes/tab.cpp
@@ -0,0 +1,24 @@
+//  learn about horizontal and vertical tab
+//  horizontal tab --> \t
+// vertical tab --> \v
+
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+
+    cout << "HORIZONTAL TAV (\\t) DEMO\n";
+    cout << "Name\tAge\tCity\n";
+    cout << "Vikas\t21\tMumbai\n";
+    cout << "Aman\t22222\tDelhi\n";
+    cout << "Manoj\t23\tJaipur\n";
+
+    cout << "\n---------------------\n\n";
+
+    cout << "VERTICAL TAB (\\v) DEMO \n";
+    cout << "LIne 1\vLine 2\vLine 3\n";
+    cout << "After vertical tabs\n";
+
+    return 0;
+}
