1b009523d298084ffb3245655d667ec2500bb8aa
VikasVk03 <vishwakarmavikas0102@gmail.com>
2026-01-16 06:43:38 +0000

    Number-Reading loops here we set loop to take only number input and if not number loop terminates . and after that we set loop to clear cin and reset for another number input

---
+++ 03-ControlStatement-Operators/34cingolf.cpp
@@ -0,0 +1,42 @@
+// cingolf.cpp  -- non-numeric input skipped
+
+#include <iostream>
+
+const int MAX = 5;
+
+int main()
+{
+    using namespace std;
+
+    // get data
+    int golf[MAX];
+
+    cout << "Please enter your golf scores.\n";
+    cout << "You must enter " << MAX << " rounds.\n";
+
+    int i;
+    for (i = 0; i < MAX; i++)
+    {
+        cout << "round #" << i + 1 << ": ";
+        while (!(cin >> golf[i]))
+        {
+            cin.clear(); // reset input
+            while (cin.get() != '\n')
+                continue;
+            cout << "Please enter a number: ";
+        }
+    }
+
+    // calculate average
+
+    double total = 0.0;
+
+    for (i = 0; i < MAX; i++)
+        total += golf[i];
+
+    // report results
+    cout << total / MAX << " = average score "
+         << MAX << " rounds\n";
+
+    return 0;
+}
